9ab40824b843b99f15acc29ebb396ff42b691c08
Errorld <errorld@outlook.com>
2015-09-22 08:27:28 -0700

    begin to work a little

--- pa.py
+++ pa.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python3
+#!usr\bin\env python3
 #coding: utf-8
 
 import tornado.httpclient
@@ -11,15 +11,20 @@ def grab_html(url):
 	cli = tornado.httpclient.HTTPClient()
 	data = cli.fetch(url)
 	body = data.body.decode('utf-8')
-	print(body)
-def find_name():
-	pass
-
+	return body
+#	print(body)
+def find_name(html):
+	soup= BeautifulSoup(html, 'html.parser')
+	name= soup.find(id='space-owner-name').get_text()
+	print(name)
+	return name
 for UID in UIDS:
 	url=head_url+str(UID)
 	print(UID)
 	html=grab_html(url)
-#	name=find_name(html)
-#	print(name)
+	name=find_name(html)
+	print(name)
 	
-	
+#url = head_url+str(1)	
+#html=grab_html(url)
+#print(html)
