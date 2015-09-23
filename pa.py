a2391e10a3de6a649d0bc30d8f364a38965590cf
Errorld <errorld@outlook.com>
2015-09-22 20:43:02 -0700

    turn to work on tittle

--- pa.py
+++ pa.py
@@ -1,6 +1,7 @@
 #!usr\bin\env python3
 #coding: utf-8
-
+import urllib
+import urllib.request
 import tornado.httpclient
 from bs4 import BeautifulSoup
 
@@ -8,11 +9,16 @@ head_url='http://space.bilibili.com/'
 UIDS=range(1,15111111)
 
 def grab_html(url):
-	cli = tornado.httpclient.HTTPClient()
-	data = cli.fetch(url)
-	body = data.body.decode('utf-8')
+	user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windos NT)'
+	#urllib.request.urlencode(user_agent)
+	headers = {'User-Agent': user_agent}	
+	req = urllib.request.Request(url, headers=headers)
+	response = urllib.request.urlopen(req)
+	print(response)
+	data = response.read()
+	body = data.decode('utf-8')
 	return body
-#	print(body)
+	print(body)
 def find_name(html):
 	soup= BeautifulSoup(html, 'html.parser')
 	name= soup.find(id='space-owner-name').get_text()
